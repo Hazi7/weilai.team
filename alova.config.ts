d302978c48a62346b4cb3d7cbbd009e7bdca0701
Hazi <83254065@qq.com>
2024-10-21 16:14:45 +0800

    init：初始化仓库

---
+++ alova.config.ts
@@ -0,0 +1,26 @@
+export default {
+	generator: [
+		{
+			input: "https://api.kuleu.com/api/suiji_renshe",
+			platform: "swagger",
+			output: "src/api",
+			responseMediaType: "application/json",
+			bodyMediaType: "application/json",
+			version: "auto",
+			type: "auto",
+			global: "Apis",
+			handleApi: (apiDescriptor) => {
+				if (!apiDescriptor.path.startWith("/user")) {
+					return;
+				}
+				apiDescriptor.parameter = apiDescriptor.parameter.filter(
+					(param) => param.in === "header" && param.name === "token",
+				);
+				apiDescriptor.requestBody.id = undefined;
+				apiDescriptor.url = apiDescriptor.url.replace("/user", "");
+				return apiDescriptor;
+			},
+		},
+	],
+	autoUpdate: true,
+};
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  ds
+</template>
+
+<style lang="scss" scoped></style>
