685c6a2682c04e957beadfbefcbe19b44f7c5757
Hazi <83254065@qq.com>
2024-10-22 14:27:13 +0800

    feat：引入 shadcn-ui 组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
-  dssddsdsds
+    <Button class="absolute left-4">Button</Button>
 </template>
 
 <style lang="scss" scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,36 @@
 import { defineConfig } from "vite";
+import { resolve } from "node:path";
+
 import vue from "@vitejs/plugin-vue";
-import path, { resolve } from "node:path";
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
 
 export default defineConfig({
+	css: {
+		postcss: {
+			plugins: [tailwind(), autoprefixer()],
+		},
+		preprocessorOptions: {
+			scss: {
+				api: "modern-compiler"
+			}
+		}
+	},
 	plugins: [vue()],
 	build: {
 		rollupOptions: {
 			input: {
-				main: path.resolve(__dirname, "./index.html"),
-				console: path.resolve(__dirname, "./src/modules/console/index.html"),
-				recruitment: path.resolve(
-					__dirname,
-					"./src/modules/recruitment/index.html",
-				),
+				main: resolve(__dirname, "./index.html"),
+				console: resolve(__dirname, "./src/modules/console/index.html"),
+				recruitment: resolve(__dirname, "./src/modules/recruitment/index.html"),
 			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": resolve(__dirname, "./src"),
+			"@console": resolve(__dirname, "./src/modules/console"),
+			"@recruitment": resolve(__dirname, "./src/modules/recruitment"),
 		},
 	},
 });
