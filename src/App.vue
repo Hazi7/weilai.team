3a618abd9434c1b936fda4bcafc68183fbfba2be
Qchengju <2804033984@qq.com>
2024-11-04 11:18:12 +0800

    feat:  引入Card组件，使用测试

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
+import { Button } from '@/components/ui/button';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle, } from '@/components/ui/card';
 </script>
 
 <template>
-    <Button class="absolute left-4">Button</Button>
+
+    <Card class="Card">
+    <CardHeader>
+      <CardTitle>组件的基本导入使用</CardTitle>
+      <CardDescription>卡片组件的描述</CardDescription>
+    </CardHeader>
+    <CardContent>
+    卡片组件的内容填充区
+    </CardContent>
+    <CardFooter>
+     卡片组件的表尾
+        <Button class="absolute left-4">Button</Button>
+    </CardFooter>
+  </Card>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.Card{
+    width: 300px;
+    height: 300px;
+    background-color: #fff;
+    margin: auto;
+    border-radius: 10px;
+}
+</style>
