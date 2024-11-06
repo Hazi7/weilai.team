cf5d128c41ac21f0e91803deeeccd46e55bed62b
Qchengju <2804033984@qq.com>
2024-11-06 20:09:31 +0800

    chore: 删除未使用的组件和配置
    - 移除了 App.vue 中未使用的 Card 组件
    - 删除了 console 和 recruitment 模块的 App.vue 和 index.html 文件
    - 移除了 vite.config.ts 文件

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,10 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button';
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle, } from '@/components/ui/card';
 </script>
 
 <template>
 
-    <Card class="Card">
-    <CardHeader>
-      <CardTitle>组件的基本导入使用</CardTitle>
-      <CardDescription>卡片组件的描述</CardDescription>
-    </CardHeader>
-    <CardContent>
-    卡片组件的内容填充区
-    </CardContent>
-    <CardFooter>
-     卡片组件的表尾
-        <Button class="absolute left-4">Button</Button>
-    </CardFooter>
-  </Card>
+
 </template>
 
 <style lang="scss" scoped>
-.Card{
-    width: 300px;
-    height: 300px;
-    background-color: #fff;
-    margin: auto;
-    border-radius: 10px;
-}
 </style>
