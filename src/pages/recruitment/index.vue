53a945dc05d2a9d58bffe588029fb043f3e3f33e
Qchengju <2804033984@qq.com>
2024-11-09 15:27:52 +0800

    feat(recruitment): 创建招新页面组件
    - 新增 Card 组件用于展示招新信息
    - 添加 Input 组件用于用户输入邮箱
    - 设置页面样式，包括卡片布局和图片
    - 更新 HTML 语言设置为中文
    - 修复 pre-commit 脚本路径

--- src/pages/recruitment/index.vue
+++ src/pages/recruitment/index.vue
@@ -1,5 +1,59 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Input } from '@/components/ui/input';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
-<template>招新</template>
+// import { Sliderbar, SliderbarItem } from '@/components/ui/sliderbar';
+</script>
 
-<style lang="scss" scoped></style>
+<template>
+
+<Card class="card">
+    <CardHeader>
+      <CardTitle><Icon icon="mdi-light:home" />贝利亚大王</CardTitle>
+      <CardDescription>\\\\\\\\</CardDescription>
+    </CardHeader>
+    <CardContent>
+    <img class="img" src="https://jsd.onmicrosoft.cn/gh/Qchengju/MyPic@img/img/202411071528493.jpg"
+    alt=""
+        />
+    </CardContent>
+    <CardFooter>
+        <Input
+    type="email"
+    placeholder="Email"
+    />
+    </CardFooter>
+  </Card>
+
+
+
+</template>
+<style lang="scss" scoped>
+    .card{
+    width: 400px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color:skyblue;
+    border: 1px solid #fff;
+    box-shadow: 0 0 10px #000;
+    text-align: center;
+    position: relative;
+    top: 200px;
+
+}
+    .img{
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        box-shadow: 0 0 10px #000;
+        margin: 0 auto;
+    }
+
+</style>
