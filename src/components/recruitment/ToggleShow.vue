578f2d4726419c94ad9f522abb42749ae5850ca5
Qchengju <2804033984@qq.com>
2024-11-14 20:05:29 +0800

    feat(recruitment): 实现招新管理系统首页布局
    - 添加 HeaderNav 组件作为顶部导航栏
    - 实现左侧快捷查看和快捷操作区域
    - 添加内容区域布局
    - 优化页面样式和响应式布局

---
+++ src/components/recruitment/ToggleShow.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- 招新顶部导航栏 -->
+    <Menubar class="border-b">
+        <MenubarMenu>
+                <MenubarTrigger><Button>待我反馈</Button></MenubarTrigger>
+                <MenubarTrigger><Button>我录取的</Button></MenubarTrigger>
+                <MenubarTrigger><Button>我淘汰的</Button></MenubarTrigger>
+        </MenubarMenu>
+    </Menubar>
+</template>
+<script setup lang="ts">
+// import { Icon } from '@iconify/vue';
+import {
+    Menubar,
+    MenubarMenu,
+    MenubarTrigger,
+} from '@/components/ui/menubar'
+// import { Badge } from '@/components/ui/badge'
+import { Button } from '@/components/ui/button';
+</script>
+<style lang="scss">
+    .badge{
+    background-color:none;
+    padding: 8px;
+    border:solid 1px #CAEAF3;
+    margin-bottom: 5px;
+    }
+    #top-nav-icon{
+        font-size: 1.2rem;
+        margin-right: 8px;
+    }
+</style>
--- src/pages/recruitment/index.vue
+++ src/pages/recruitment/index.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { Input } from '@/components/ui/input';
 import {
   Card,
   CardContent,
@@ -8,52 +7,263 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
+import {HeaderNav} from '@/components/recruitment';
+import { Icon } from '@iconify/vue';
+// import { ToggleShow } from '@/components/recruitment';
+document.title = '招新管理';
 
-// import { Sliderbar, SliderbarItem } from '@/components/ui/sliderbar';
 </script>
 
 <template>
+<div class="main">
+  <header-nav id="header"></header-nav>
 
-<Card class="card">
-    <CardHeader>
-      <CardTitle><Icon icon="mdi-light:home" />贝利亚大王</CardTitle>
-      <CardDescription>\\\\\\\\</CardDescription>
-    </CardHeader>
-    <CardContent>
-    <img class="img" src="https://jsd.onmicrosoft.cn/gh/Qchengju/MyPic@img/img/202411071528493.jpg"
-    alt=""
-        />
-    </CardContent>
-    <CardFooter>
-        <Input
-    type="email"
-    placeholder="Email"
-    />
-    </CardFooter>
-  </Card>
+  <div id="left-side" >
+    <div id="quick-look-group">
+        <Card class="card-message-show">
+        <CardHeader >
+          <CardTitle> </CardTitle>
+          <CardDescription>待安排</CardDescription>
+        </CardHeader>
+        <CardContent>
+          6
+        </CardContent>
+        <CardFooter>
+          <Icon icon="lucide:hand" />
+        </CardFooter>
+      </Card>
+      <Card class="card-message-show">
+        <CardHeader >
+          <CardTitle></CardTitle>
+          <CardDescription> 已录取<Icon icon="lucide:hand" /></CardDescription>
+        </CardHeader>
+        <CardContent>
+          6
+        </CardContent>
+        <CardFooter>
+        </CardFooter>
+      </Card>
+    </div>
+    <div id="quick-control" >
+          <Card id="card-control">
+            <CardHeader >
+              <CardTitle id="title">快捷操作 </CardTitle>
+              <CardDescription></CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div id="control-group" >
+                <div id="control-item">
+                    <div id="control-icon"><Icon id="control-icon-svg" icon="mdi-light:vector-arrange-below"/></div>
+                    <div  id="control-text">
+                      <h6>安排面试</h6>
+                      <p>候选人</p>
+                    </div>
+                    <div id="control-click">
+                      <Icon id="control-click-svg" icon="solar:alt-arrow-right-line-duotone" />
+                    </div>
+                </div>
 
+                <div id="control-item">
+                    <div id="control-icon"><Icon id="control-icon-svg" icon="mdi-light:vector-arrange-below"/></div>
+                    <div  id="control-text">
+                      <h6>安排面试</h6>
+                      <p>候选人</p>
+                    </div>
+                    <div id="control-click">
+                      <Icon id="control-click-svg" icon="solar:alt-arrow-right-line-duotone" />
+                    </div>
+
+                </div>
+
+              </div>
+
+            </CardContent>
+            <CardFooter>
+            </CardFooter>
+          </Card>
+    </div>
+
+  </div>
+
+   <div id="content">
+
+    <div id="content-container" >
+
+    </div>
+   </div>
+
+   </div>
 
 
 </template>
 <style lang="scss" scoped>
-    .card{
-    width: 400px;
-    margin: 0 auto;
-    padding: 20px;
-    background-color:skyblue;
-    border: 1px solid #fff;
-    box-shadow: 0 0 10px #000;
-    text-align: center;
+  .main{
+    width: calc(100% - 200px);
+    height: auto;
+    background-color: #fff;
     position: relative;
-    top: 200px;
-
+    left: 200px;
+    display:inline-block;
+    top: 20px;
+    box-sizing: content-box;
+    @media screen and (max-width: 768px) {
+        width: 100% ;
+        left: 0px;
 }
-    .img{
-        width: 100px;
-        height: 100px;
-        border-radius: 50%;
-        box-shadow: 0 0 10px #000;
-        margin: 0 auto;
+  }
+
+// #region 头部导航栏
+  #header{
+    width: 100%;
+    height: 50px;
+    background-color: #fff;
+    position: relative;
+  }
+// #endregion
+
+//#region 左侧导航栏
+
+  #left-side{
+    width: 20%;
+    height: auto;
+    background-color: #fff;
+    position: relative;
+    display: inline-block;
+    top: 20px;
+    margin-top: 10px ;
+    border: none;
+
+  }
+  .card-message-show{
+    width: 100%;
+    height: 150px;
+    background-color: #fff;
+    position: relative;
+    display: inline-block;
+    margin-top: 10px ;
+  }
+  #quick-look-group{
+    width: 100%;
+    background-color: #fff;
+    position: relative;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  #quick-control{
+    width: 100%;
+    background-color: #fff;
+    position: relative;
+    display:flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    #card-control{
+      width: 90%;
+      height: 150px;
     }
+  }
+  #quick-control{
+    width: 100%;
+    height: 150px;
+    background-color: #fff;
+    position: relative;
+    display: inline-block;
+    top: 20px;
+    margin-top: 10px ;
+    border: none;
+    #card-control{
+      width: 100%;
+      height: auto;
+      border: none;
+      #title{
+        font-size: 1em;
+        font-weight: 100;
+        text-align: left;
+      }
+      #control-group{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        #control-item{
+        display: flex;
+        flex-direction: row;
+          #control-icon{
+            width: 60px;
+            height: 40px;
+            background-color:#CDD9FC;
+            position: relative;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 10%;
+            margin-bottom: 20px;
+            #control-icon-svg{
+            width: 20px;
+            height: 20px;
+            position: relative;
+            display: inline-block;
+            border: none;
+            border-radius: 10%;
+            color: white;
+           }
+          }
+          #control-text{
+            width: 100%;
+            height: 40px;
+            background-color: #fff;
+            position: relative;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            text-align: left;
+            margin-left: 10px;
+            p{
+              font-size: 0.8em;
+              color: #B1B2C6;
+            }
+          }
+
+          #control-click{
+            width: 60px;
+            height: 40px;
+            position: relative;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-radius: 10%;
+            background-color: #F0F0F0;
+            #control-click-svg{
+           font-size: 1.2em;
+            }
+          }
+
+        }
+      }
+
+    }
+
+  }
+//#endregion
+
+
+//#region 中间内容区
+  #content{
+    width: 70%;
+    background-color: #4d2020;
+    position: relative;
+    display: inline-block;
+    top: 20px;
+    border: none;
+  }
+  #content-container{
+    width: 100%;
+    height: auto;
+  }
+
+
+//#endregion
 
 </style>
