b0330ad472230e1a6c530dbfe9f4154b55ee2d77
wdlxxx <2206655687@qq.com>
2024-11-22 16:11:53 +0800

    feat:获取帖子信息并渲染页面，新建useDateFormatter.ts转换ios格式的日期

--- src/features/personalCenter/pages/userInfo/myPosts/index.vue
+++ src/features/personalCenter/pages/userInfo/myPosts/index.vue
@@ -2,38 +2,38 @@
     <div class="myPosts">
         <div class="postsData">
             <div class="postsNum">
-                <p>0</p>
+                <p>{{ userPostAllInfo.allPostCount }}</p>
                 <p>文章</p>
             </div>
             <div class="likeNum">
-                <p>0</p>
+                <p>{{ userPostAllInfo.allLikeCount }}</p>
                 <p>点赞</p>
             </div>
             <div class="starNum">
-                <p>0</p>
+                <p>{{ userPostAllInfo.allCollectCount }}</p>
                 <p>收藏</p>
             </div>
             <div class="commentNum">
-                <p>0</p>
+                <p>{{ userPostAllInfo.allCommentCount }}</p>
                 <p>评论</p>
             </div>
         </div>
         <div class="postsListBox">
             <ul>
-                <li v-for="(item, index) in 5" :key="index">
+                <li v-for="item  in userPost" :key="item.id">
                     <img src="@/assets/img/test.jpg" alt="文章封面">
                     <div class="postInfo">
-                        <h1 class="postTitle">文章标题</h1>
+                        <h1 class="postTitle">{{ item.title }}</h1>
                         <p class="postFooter">
-                            <span class="postTime">2020-01-01</span>
+                            <span class="postTime">{{ formatDateToYYYYMMDD(item.putTime)}}</span>
                             ·
-                            <span class="likesNum">23 点赞</span>
+                            <span class="likesNum">{{ item.postLikeCount }} 点赞</span>
                             ·
-                            <span class="commentsNum">23 评论</span>
+                            <span class="commentsNum">{{ item.commentCount }} 评论</span>
                             ·
-                            <span class="collectNum">23 收藏</span>
+                            <span class="collectNum">{{ item.collectCount }} 收藏</span>
                             ·
-                            <span class="viewNum">23 阅读</span>
+                            <span class="viewNum">{{ item.viewCount }} 阅读</span>
                         </p>
                     </div>
                     <DropdownMenu>
@@ -53,9 +53,29 @@
                 </li>
             </ul>
         </div>
+        <div class="pageBox">
+            <Pagination v-slot="{ page }" :total="pages * 10" :sibling-count="1" show-edges :default-page="1"
+                @update:page="handlePageChange">
+                <PaginationList v-slot="{ items }" class="flex items-center gap-1">
+                    <PaginationFirst />
+                    <PaginationPrev />
+                    <template v-for="(item, index) in items">
+                        <PaginationListItem v-if="item.type === 'page'" :key="index" :value="item.value" as-child>
+                            <Button class="w-10 h-10 p-0" :variant="item.value === page ? 'default' : 'outline'">
+                                {{ item.value }}
+                            </Button>
+                        </PaginationListItem>
+                        <PaginationEllipsis v-else :key="item.type" :index="index" />
+                    </template>
+
+                    <PaginationNext />
+                    <PaginationLast />
+                </PaginationList>
+            </Pagination>
+        </div>
     </div>
 </template>
-<script lang="ts"  setup>
+<script lang="ts" setup>
 import { Icon } from '@iconify/vue';
 import {
     DropdownMenu,
@@ -66,12 +86,50 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu'
 import Button from '@/components/ui/button/Button.vue';
+import {
+    Pagination,
+    PaginationEllipsis,
+    PaginationFirst,
+    PaginationLast,
+    PaginationList,
+    PaginationListItem,
+    PaginationNext,
+    PaginationPrev,
+} from '@/components/ui/pagination'
+import { ref } from 'vue'
+import { useRequest } from '@/composables/useRequest'
+const { data, error, loading, executeRequest } = useRequest()
+import { useLocalStorageWithExpire } from '@/composables/useLocalStorage';
+const { getLocalStorageWithExpire, setLocalStorageWithExpire } = useLocalStorageWithExpire()
+import {useDateFormatter} from '@/composables/useDateFormatter'
+const {formatDateToYYYYMMDD} = useDateFormatter()
+
+
+const userId = getLocalStorageWithExpire('userId')
+let userPostAllInfo = ref({
+    allCollectCount: 0,
+    allCommentCount: 0,
+    allLikeCount: 0,
+    allPostCount: 0,
+})
+//定义
+let pages = 0;
+let currentPage = 1;
+let userPost= ref([]);
+function handlePageChange(newPage: number) {
+    currentPage = newPage;
+    getPosts()
+}
 
 async function getPosts() {
-    // await executeRequest({ url: `/user/getUserInfoByUserId/${userId}` })
-    // if (data.value && data.value.code == 200) {
-    //     Object.assign(userInfo, data.value.data);
-    // }
+    await executeRequest({ url: `/user/getUserPost?userId=${userId}&pageNumber=${currentPage}&pageSize=5` })
+    console.log(data.value);
+    if (data.value && data.value.code == 200) {
+        let postData = data.value.data
+        Object.assign(userPostAllInfo.value, postData.userPostAllInfo);
+        userPost.value=postData.userPost
+        pages = postData.pageInfo.pages
+    }
 }
 getPosts()
 </script>
@@ -120,19 +178,22 @@ getPosts()
                 padding: 10px;
                 box-shadow: 5px 5px 5px #d9d9d9;
                 border: 1px solid #d9d9d9;
+
                 img {
                     width: 80px;
                 }
-                
+
                 .postInfo {
                     margin-left: 20px;
                     width: 100%;
                     color: #666;
-                    .postTitle{
+
+                    .postTitle {
                         height: 50px;
                         font-size: larger;
                     }
-                    .postFooter{
+
+                    .postFooter {
                         height: 20px;
                     }
                 }
@@ -140,10 +201,20 @@ getPosts()
                 .ellipsis {
                     margin-right: 20px;
                 }
+
                 &:hover {
                     background-color: #f5f5f5;
                 }
             }
+        }
+    }
+
+    .pageBox {
+        margin: 20px auto;
+        width: 450px;
+
+        button[data-selected] {
+            background-color: #97d5ff;
         }
     }
 }
