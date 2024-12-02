1c38a9e0b8dbb5793a13b3b70c47cac1ec1c7ded
mq <1596684751@qq.com>
2024-12-02 19:45:16 +0800

    feat(tag):退出登录，登录页面样式修改

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import LoginForm from '@/components/login/LoginForm.vue'
+import LoginAnimation from '@/components/login/LoginAnimation.vue';
 </script>
 
 <template>
+    <LoginAnimation></LoginAnimation>
     <LoginForm></LoginForm>
 </template>
