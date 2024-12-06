5a9e6ae6fdc2938fcb4b8d0f4e3597853d8b85b3
Hazi <83254065@qq.com>
2024-12-06 16:54:20 +0800

    refactor：调整token传参位置

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -4,8 +4,9 @@ import axios, {
   type AxiosResponse,
 } from "axios";
 import { useLocalStorageWithExpire } from "../composables/useLocalStorage";
-import router from '@/router';
-const {setLocalStorageWithExpire,getLocalStorageWithExpire}=useLocalStorageWithExpire()
+import router from "@/router";
+const { setLocalStorageWithExpire, getLocalStorageWithExpire } =
+  useLocalStorageWithExpire();
 const apiClient: AxiosInstance = axios.create({
   baseURL: "http://49.232.183.67:8087",
   timeout: 10000,
@@ -17,10 +18,10 @@ const apiClient: AxiosInstance = axios.create({
 // 添加请求拦截器
 apiClient.interceptors.request.use(
   (config: InternalAxiosRequestConfig) => {
-    const token = getLocalStorageWithExpire('token');
+    const token = getLocalStorageWithExpire("token");
     if (token && config.headers) {
-      setLocalStorageWithExpire('token',token,1000*60*60)
-      config.headers.token = token;
+      setLocalStorageWithExpire("token", token, 1000 * 60 * 60);
+      config.headers.Authorization = `Bearer ${token}`;
     }
     console.log(config);
     return config;
@@ -34,8 +35,8 @@ apiClient.interceptors.request.use(
 apiClient.interceptors.response.use(
   (response: AxiosResponse) => {
     if (response.data.code === 401) {
-      console.log('token过期或未登录');
-      router.push('/login')
+      console.log("token过期或未登录");
+      router.push("/login");
     }
     return response.data;
   },
